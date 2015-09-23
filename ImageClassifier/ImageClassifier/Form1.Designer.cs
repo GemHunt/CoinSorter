081fdc2e64e6dc5fc041e3e67565de642c669829
Paul Krush <pkrush@gemhunt.com>
2015-09-23 16:58:41 -0500

    classification.dll works

--- ImageClassifier/ImageClassifier/Form1.Designer.cs
+++ ImageClassifier/ImageClassifier/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(442, 245);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
--- ImageClassifier/ImageClassifier/Form1.cs
+++ ImageClassifier/ImageClassifier/Form1.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace ImageClassifier
 {
     public partial class Form1 : Form
     {
+        [DllImport("D:\\GitHub\\build\\Caffe-prefix\\src\\Caffe-build\\examples\\cpp_classification\\Debug\\classification-d.dll")]
+        public static extern int ClassifyNetwork(String model_file, String trained_file, String mean_file, String label_file, String image_file);
+
+        
         public Form1()
         {
             InitializeComponent();
@@ -19,11 +24,16 @@ namespace ImageClassifier
 
         private void button1_Click(object sender, EventArgs e)
         {
+           String dir = "F:\\model\\";
 
+           String model_file = dir + "deploy.prototxt";
+           String trained_file = dir + "snapshot_iter_11836.caffemodel";
+           String mean_file = dir + "mean.binaryproto";
+           String label_file = dir + "labels.txt";
+            String image_file = "F:\\Rotated\\HeadsWithRotation360\\000\\1000000251020612.png";
 
-
-
-
+            int result = ClassifyNetwork(model_file, trained_file, mean_file, label_file, image_file);
+            Console.WriteLine(result);
         }
     }
 }
