8212bd275cb9a966733a4bbe13d8d3159a126d36
Paul Krush <pkrush@gemhunt.com>
2015-10-15 15:37:07 -0500

    Actuator with IR Sensor working.

--- hardware/actuators/actuator/actuator.ino
+++ hardware/actuators/actuator/actuator.ino
@@ -4,23 +4,58 @@ AF_DCMotor motor1(1, MOTOR12_64KHZ); // create motor #1, 64KHz pwm
 AF_DCMotor motor2(2, MOTOR12_64KHZ); // create motor #2, 64KHz pwm
 String inputString = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
- 
+int toggleCountdown = 0;
+int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorValue = 0;  // variable to store the value coming from the sensor
+int offCount = 0;
+
+
 void setup() {
-  Serial.begin(250000);           // set up Serial library at 9600 bps
-  Serial.println("Motor test!");
+  Serial.begin(250000);           
   motor1.setSpeed(255);     // set the speed to 200/255
   motor2.setSpeed(255);     // set the speed to 200/255
   inputString.reserve(10);
 }
  
 void loop() {
-  serialEvent(); 
+   readIRSensor();
+   serialEvent(); 
  if (stringComplete) {
-    toggle();
+    toggle(true);
+    toggleCountdown = 2000;
     inputString = "";
     stringComplete = false;
   }
+  if (toggleCountdown != 0) {
+    if (toggleCountdown == 1) {
+      toggleCountdown = 0;
+      toggle(false);
+    }
+  else {toggleCountdown--;}
+  }
 }
+
+void readIRSensor() {
+  //Serial.println(analogRead(sensorPin));
+  
+  int irSensorVoltageCutoff = 900;
+  // the average power of the sensor through an ever more dirty belt. 
+  // Heck this is the dirty belt sensor!
+  //irSensorVoltageCutoff needs to adjust, maybe it should be set on the PC side?
+  if (analogRead(sensorPin) > irSensorVoltageCutoff)  {
+      offCount = offCount + 1;
+    }
+  else
+  {
+  if (offCount != 0) {
+      //if it's under 50 it's just a bounce. 
+      if (offCount > 50) {
+        Serial.println(offCount );
+      }
+      offCount = 0;
+      }
+  } 
+} 
 
 void serialEvent() {
   while (Serial.available()) {
@@ -36,13 +71,17 @@ void serialEvent() {
   }
 }
 
-void toggle() {
-  motor1.run(FORWARD);      // turn it on going forward
-  motor2.run(FORWARD);      // turn it on going forward
-  delay(50);
-  motor1.run(RELEASE);      // stopped
-  motor2.run(RELEASE);      // stopped
+void toggle(boolean on) {
+  if (on) {
+    motor1.run(FORWARD);      // turn it on going forward
+    motor2.run(FORWARD);      // turn it on going forward
+  }
+  else {
+    motor1.run(RELEASE);      // stopped
+    motor2.run(RELEASE);      // stopped  
+   }
 }
+
 
 
 
