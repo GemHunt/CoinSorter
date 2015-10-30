f5ca5ad39c99b8bee801e632f2dc75bfc1c82d5b
Paul Krush <pkrush@gemhunt.com>
2015-10-30 12:54:20 -0500

    Added Classes, getting code out of forms!

---
+++ ImageClassifier/CaffeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Runtime.InteropServices;
+using System.IO;
+
+
+namespace ImageClassifier
+{
+    class CaffeModel
+    {
+
+
+
+    }
+}
