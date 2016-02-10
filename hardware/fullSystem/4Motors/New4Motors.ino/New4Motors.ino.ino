f132f9496b110309c3ca125fb8f194adacf8a1b5
Paul Krush <pkrush@gemhunt.com>
2016-02-10 15:59:01 -0600

    Scanning Conveyor Updates

--- hardware/fullSystem/4Motors/New4Motors.ino/New4Motors.ino.ino
+++ hardware/fullSystem/4Motors/New4Motors.ino/New4Motors.ino.ino
@@ -9,7 +9,7 @@ void setup() {
   Serial.begin(250000);           
    motor1.setSpeed(255);     // set the speed to 200/255
   motor2.setSpeed(255);     // set the speed to 200/255
-  motor3.setSpeed(255);     // set the speed to 200/255
+  //motor3.setSpeed(255);     // set the speed to 200/255
   //motor4.setSpeed(255);     // set the speed to 200/255
    //motor1.run(FORWARD);      // turn it on going forward
     //motor2.run(FORWARD);      // turn it on going forward
@@ -19,14 +19,16 @@ void loop() {
  //for (int i=0; i <= 10; i++){
     motor1.run(FORWARD);      // turn it on going forward
     motor2.run(FORWARD);      // turn it on going forward
-    motor3.run(FORWARD);      // turn it on going forward
+    //motor3.run(FORWARD);      // turn it on going forward
     //motor4.run(FORWARD);      // turn it on going forward
     delay(50); 
     //motor1.run(BACKWARD);      // turn it on going forward
-    //motor1.run(RELEASE);      // turn it on going forward
-    //motor2.run(RELEASE);      // turn it on going forward
+    //motor2.run(BACKWARD);      // turn it on going forward
+    motor1.run(RELEASE);      // turn it on going forward
+    delay(15); 
+    motor2.run(RELEASE);      // turn it on going forward
    //motor3.run(RELEASE);      // turn it on going forward
-    delay(50); 
+    delay(40); 
     //} 
 //motor1.run(RELEASE); 
 //delay(500);
