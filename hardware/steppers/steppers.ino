932ed5a68f232f7bfd59043a9a82c3952d248fea
Paul Krush <pkrush@gemhunt.com>
2016-09-14 11:13:15 -0500

    Add unipolar code

--- hardware/steppers/steppers.ino
+++ hardware/steppers/steppers.ino
@@ -33,3 +33,63 @@ void loop() {
   motor.step(250, FORWARD, MICROSTEP); 
   motor.step(250, BACKWARD, MICROSTEP); 
 }
+
+
+
+
+/*
+Version that works for unipolor motors, but I wounder if there is a function for this as well. 
+AF_DCMotor motor1(1, MOTOR12_64KHZ); // create motor #1, 64KHz pwm
+AF_DCMotor motor2(2, MOTOR12_64KHZ); // create motor #2, 64KHz pwm
+AF_DCMotor motor3(3, MOTOR12_64KHZ); // create motor #2, 64KHz pwm
+AF_DCMotor motor4(4, MOTOR12_64KHZ); // create motor #2, 64KHz pwm
+int d;
+
+void setup() {
+  Serial.begin(250000);
+  motor1.setSpeed(255);     
+  motor2.setSpeed(255);     
+  motor3.setSpeed(255);     
+  motor4.setSpeed(255);     
+  d = 4;
+}
+
+void loop() {
+ 
+  motor1.run(FORWARD);      // turn it on going forward
+  delay(d);
+  motor4.run(RELEASE);      // turn it on going forward
+  delay(d);
+  motor2.run(FORWARD);      // turn it on going forward
+  delay(d);
+  motor1.run(RELEASE);      // turn it on going forward
+  delay(d);
+  motor3.run(FORWARD);      // turn it on going forward
+  delay(d);
+  motor2.run(RELEASE);      // turn it on going forward
+  delay(d);
+  motor4.run(FORWARD);      // turn it on going forward
+  delay(d);
+  motor3.run(RELEASE);      // turn it on going forward
+  delay(d);
+  
+  
+  /*
+  delay(10);
+  motor1.run(RELEASE);      // turn it on going forward
+  motor2.run(FORWARD);      // turn it on going forward
+  delay(10);
+  motor1.run(BACKWARD);      // turn it on going forward
+  motor2.run(RELEASE);      // turn it on going forward
+  delay(10);
+  motor1.run(RELEASE);      // turn it on going forward
+  motor2.run(BACKWARD);      // turn it on going forward
+  delay(10);
+  */
+}
+*/
+
+
+
+
+
