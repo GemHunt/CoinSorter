af946893cf3d4fd3942a536bc7e3c34b7a430307
Paul Krush <pkrush@GemHunt.com>
2017-01-26 19:48:08 -0600

    added 2 belt control for twirling coins for close up top and bottom camera captures

--- hardware/steppers/steppers.ino
+++ hardware/steppers/steppers.ino
@@ -12,7 +12,7 @@ void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Stepper test!");
 
-  motor.setSpeed(150);  // rpm
+  motor.setSpeed(100);  // rpm
 }
 
 void loop() {
@@ -30,7 +30,8 @@ void loop() {
   motor.step(100, BACKWARD, INTERLEAVE); 
 */
   //Serial.println("Micrsostep steps");
-  motor.step(165, FORWARD, MICROSTEP); 
+  motor.step(1, FORWARD, DOUBLE); 
   //motor.step(110, BACKWARD, MICROSTEP); 
+//  delay(100);
 }
 
